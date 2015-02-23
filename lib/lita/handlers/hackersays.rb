b0103e91adcb889139c6cbf1e8eae7bbfdc70b7c
Jarmo Pertman <jarmo.p@gmail.com>
2015-02-23 18:00:55 +0200

    Add routes.

--- lib/lita/handlers/hackersays.rb
+++ lib/lita/handlers/hackersays.rb
@@ -1,6 +1,13 @@
 module Lita
   module Handlers
     class Hackersays < Handler
+      route %r{^hacker(says?|quote)$}i, :command, command: true, help: {
+        "hackersays" => "Returns a random quote from a hacker."
+      }
+
+      def command(response)
+
+      end
     end
 
     Lita.register_handler(Hackersays)
--- spec/lita/handlers/hackersays_spec.rb
+++ spec/lita/handlers/hackersays_spec.rb
@@ -1,4 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::Hackersays, lita_handler: true do
+  it { is_expected.to route_command("hackersays")}
+  it { is_expected.to route_command("hackersay")}
+  it { is_expected.to route_command("hackerquote")}
 end
