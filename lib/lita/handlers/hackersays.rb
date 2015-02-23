7911b757d9fcdb216b19e987d49037a77d45b22c
Jarmo Pertman <jarmo.p@gmail.com>
2015-02-23 19:31:24 +0200

    Fetch quotes from hackersays.

--- lib/lita/handlers/hackersays.rb
+++ lib/lita/handlers/hackersays.rb
@@ -1,3 +1,5 @@
+require "json"
+
 module Lita
   module Handlers
     class Hackersays < Handler
@@ -6,7 +8,19 @@ module Lita
       }
 
       def command(response)
+        response.reply as_text(fetch_quote)
+      rescue
+        response.reply "Sorry, no quotes from me right now. I guess all hackers are actually busy with hacking ;)"
+      end
 
+      private
+
+      def fetch_quote
+        JSON.load http.get("http://hackersays.com/quote").body
+      end
+
+      def as_text(quote)
+        "“ #{quote["c"]} — #{quote["a"]}"
       end
     end
 
--- spec/lita/handlers/hackersays_spec.rb
+++ spec/lita/handlers/hackersays_spec.rb
@@ -4,4 +4,21 @@ describe Lita::Handlers::Hackersays, lita_handler: true do
   it { is_expected.to route_command("hackersays")}
   it { is_expected.to route_command("hackersay")}
   it { is_expected.to route_command("hackerquote")}
+
+  describe "#command" do
+    it "returns quote from hackersays" do
+      expect(subject).to receive(:fetch_quote).and_return(
+        "c" => "Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it.",
+        "a" => "Alan J. Perlis"
+      )
+      send_command "hackersays"
+      expect(replies.last).to eq("“ Fools ignore complexity. Pragmatists suffer it. Some can avoid it. Geniuses remove it. — Alan J. Perlis")
+    end
+
+    it "handles errors" do
+      expect(subject).to receive(:fetch_quote).and_raise(RuntimeError.new)
+      send_command "hackersays"
+      expect(replies.last).to eq("Sorry, no quotes from me right now. I guess all hackers are actually busy with hacking ;)")
+    end
+  end
 end
